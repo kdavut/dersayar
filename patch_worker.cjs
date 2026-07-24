da0c17f8340cae3bb8d53f19f297e7f5c59c20d9
Davut KILIÇ <davutk144@gmail.com>
2026-07-25 01:39:32 +0300

    deneme

--- patch_worker.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/utils/scheduler.worker.ts', 'utf8');
-
-code = code.replace(
-  "const course = coursesMap.get(confAssignObj.courseId);\n          if (course?.isLocked) return false;",
-  "// Removed invalid course?.isLocked check"
-);
-
-fs.writeFileSync('src/utils/scheduler.worker.ts', code);
--- test-sed.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('./src/components/ScheduleTab.tsx', 'utf8').split('\n');
-for (let i = 0; i < lines.length; i++) {
-    if (lines[i].endsWith('}') && lines[i+1] === '') {
-        console.log(`Found pattern at line ${i+1}`);
-    }
-}
